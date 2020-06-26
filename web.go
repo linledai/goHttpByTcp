f19d2aa6c8614ffe85d29a49c7d803fb5d223a1e
linledai <linledai@163.com>
2020-06-26 19:04:38 +0800

    split parse get and post

--- web.go
+++ web.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"fmt"
 	"strings"
+	"strconv"
 )
 
 func main() {
@@ -25,21 +26,36 @@ func main() {
 func handlerConn(conn net.Conn) {
 	defer conn.Close()
 	buf := make([]byte, 1024 )
+	var finish = true
+	var request *httpRequest
+	var finishParseHeader = false
 	for {
 		// TODO 大消息处理
 		n, err := conn.Read(buf)
 		data := string(buf[:n])
 		if data != "" {
-			firstLineData := parseProtocol(data[0:strings.Index(data, "\r\n")])
-			fmt.Println("协议:" + firstLineData.protocol)
-			fmt.Println("方法:" + firstLineData.method)
-			indexFirst := strings.Index(data, "\r\n")
-			indexSecond := strings.Index(data, "\r\n\r\n")
-			headLineData := parseHeader(data[indexFirst+2:indexSecond])
-			if headLineData != nil {
-				request := new(httpRequest)
-				request.protocol = *firstLineData
-				request.headMap = headLineData
+			if finish {
+				request = new(httpRequest)
+				finishParseHeader = false
+			}
+			request.requestBody += data;
+			if strings.Index(request.requestBody, "\r\n") == -1 {
+				continue
+			}
+			if !finishParseHeader {
+				firstLineData := parseProtocol(request.requestBody[0:strings.Index(data, "\r\n")])
+				fmt.Println("协议:" + firstLineData.protocol)
+				fmt.Println("方法:" + firstLineData.method)
+				indexFirst := strings.Index(request.requestBody, "\r\n")
+				indexSecond := strings.Index(request.requestBody, "\r\n\r\n")
+				headLineData := parseHeader(request.requestBody[indexFirst+2:indexSecond])
+				if headLineData != nil {
+					request.protocol = *firstLineData
+					request.headMap = headLineData
+				}
+			}
+			finish = parseRequest(*request)
+			if finish {
 				response := handlerRequest(*request)
 				conn.Write([]byte(response))
 			}
@@ -56,6 +72,7 @@ type httpRequest struct {
 	headMap map[string]string
 	requestURL string
 	queryMap map[string]string
+	requestBody string
 }
 
 type firstLine struct {
@@ -87,26 +104,24 @@ func parseProtocol(protocolLine string) (*firstLine){
 	return firstLine
 }
 
-func handlerRequest(request httpRequest) (string){
-	fmt.Println("Handler httpRequest")
+func parseRequest(request httpRequest) (bool){
+	fmt.Println("Parse httpRequest")
+	fmt.Println(request.requestBody)
 	switch request.protocol.method {
 		case "GET":
-			parseURL(request)
-			break;
+			return parseGet(request)
 		case "POST":
-			parseBody()
-			break;
+			return parsePost(request)
 		default:
-			break;
+			return true;
 	}
-	return "HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nLast-Modified : Fri , 12 May 2020 18:53:33 GMT\n\rContent-Length:14\n\rContent-Type:application/json\r\n\r\n{\"test\":\"123\"}"
 }
 
-func parseURL(request httpRequest) {
+func parseGet(request httpRequest) (bool) {
 	url := request.protocol.url;
 	index := strings.Index(url, "?")
 	if index == -1 {
-		return
+		return true
 	}
 	request.requestURL = url[0:index]
 	queryParamString := url[index+1:]
@@ -122,8 +137,21 @@ func parseURL(request httpRequest) {
 	}
 	request.queryMap = queryMap
 	fmt.Println("GET METHOD PARSE QUERY PARAMS")
+	return true
 }
 
-func parseBody() {
+func parsePost(request httpRequest) (bool){
+	headMap := request.headMap
+	index := strings.Index(request.requestBody, "\r\n\r\n")
+	requestBody := request.requestBody[index+2:]
+	length, _ := strconv.ParseInt(strings.Trim(headMap["Content-Length"], " "), 0, 32)
 	fmt.Println("POST METHOD PARSE REQUEST BODY")
+	if int(length) == len(requestBody) {
+		return true
+	}
+	return false
+}
+
+func handlerRequest(request httpRequest) (string) {
+	return "HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nLast-Modified : Fri , 12 May 2020 18:53:33 GMT\r\nContent-Length:14\r\nContent-Type:application/json\r\n\r\n{\"test\":\"123\"}"
 }
